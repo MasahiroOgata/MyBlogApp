8b681cbecf7341bea344e85735c4acc3c66032b0
MasahiroOgata <ogata0801@gmail.com>
2025-06-10 19:08:31 +0900

    レイアウト修正

--- myblog/diary/views.py
+++ myblog/diary/views.py
@@ -13,6 +13,21 @@ class DiaryOwnerMixin(generic.base.ContextMixin):
         except CustomUser.DoesNotExist:
             context['diary_owner'] = None
         return context
+    
+class CollateAuthorAndPkMixin(generic.detail.SingleObjectMixin):
+    model = Article
+
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
+        username = self.kwargs.get('username')
+        return self.model.objects.filter(id=pk, author__username=username)
+
+    # def get_queryset(self):
+    #     if not self.model:
+    #         raise ImproperlyConfigured("CollateAuthorAndPkMixin requires a model.")
+    #     pk = self.kwargs.get('pk')
+    #     username = self.kwargs.get('username')
+    #     return self.model.objects.filter(id=pk, author__username=username)
 
 class IndexView(generic.ListView):
     def get_queryset(self):
@@ -22,11 +37,14 @@ class UserIndexView(DiaryOwnerMixin, generic.ListView):
     template_name = 'diary/user_index.html' 
     
     def get_queryset(self):
-        username = self.kwargs['username']
+        username = self.kwargs['username']        
         return Article.objects.filter(author__username=username).order_by('-created_at')
     
 class DetailView(DiaryOwnerMixin, generic.DetailView):
-    model = Article
+    def get_queryset(self):
+        pk = self.kwargs['pk']
+        username = self.kwargs['username']
+        return Article.objects.filter(id=pk, author__username=username)
 
 class CreateView(DiaryOwnerMixin, generic.edit.CreateView):
     model = Article
@@ -40,8 +58,7 @@ class DeleteView(DiaryOwnerMixin, generic.edit.DeleteView):
     model = Article 
      
     def get_success_url(self):
-        return reverse_lazy('diary:user_index', 
-                            kwargs={'username': self.kwargs['username']})
+        return reverse_lazy('diary:user_index', kwargs={'username': self.kwargs['username']})
 
 # def get_diary_owner(self, context):    
 #     username = self.kwargs['username']  
