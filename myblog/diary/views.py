afeac74b0e8668580e3d2728611355142a305ae4
MasahiroOgata <ogata0801@gmail.com>
2025-06-11 15:56:24 +0900

    cssフォルダ作成

--- myblog/diary/views.py
+++ myblog/diary/views.py
@@ -10,9 +10,14 @@ class DiaryOwnerMixin(generic.base.ContextMixin):
         try:
             diary_owner = CustomUser.objects.get(username=username)
             context['diary_owner'] = diary_owner
+            context['object_list'] = Article.objects.filter(author__username=username).order_by('-created_at')
         except CustomUser.DoesNotExist:
             context['diary_owner'] = None
         return context
+    
+    # def get_queryset(self):
+    #     username = self.kwargs['username']        
+    #     return Article.objects.filter(author__username=username).order_by('-created_at')
     
 class CollateAuthorAndPkMixin(generic.detail.SingleObjectMixin):
     model = Article
@@ -22,55 +27,25 @@ class CollateAuthorAndPkMixin(generic.detail.SingleObjectMixin):
         username = self.kwargs.get('username')
         return self.model.objects.filter(id=pk, author__username=username)
 
-    # def get_queryset(self):
-    #     if not self.model:
-    #         raise ImproperlyConfigured("CollateAuthorAndPkMixin requires a model.")
-    #     pk = self.kwargs.get('pk')
-    #     username = self.kwargs.get('username')
-    #     return self.model.objects.filter(id=pk, author__username=username)
-
 class IndexView(generic.ListView):
     def get_queryset(self):
         return Article.objects.order_by('-created_at')
 
 class UserIndexView(DiaryOwnerMixin, generic.ListView):
+    model = Article
     template_name = 'diary/user_index.html' 
-    
-    def get_queryset(self):
-        username = self.kwargs['username']        
-        return Article.objects.filter(author__username=username).order_by('-created_at')
-    
-class DetailView(DiaryOwnerMixin, generic.DetailView):
-    def get_queryset(self):
-        pk = self.kwargs['pk']
-        username = self.kwargs['username']
-        return Article.objects.filter(id=pk, author__username=username)
+        
+class DetailView(DiaryOwnerMixin, CollateAuthorAndPkMixin, generic.DetailView):
+    pass
 
 class CreateView(DiaryOwnerMixin, generic.edit.CreateView):
     model = Article
     fields = '__all__'
 
-class UpdateView(DiaryOwnerMixin, generic.edit.UpdateView):
-    model = Article
+class UpdateView(DiaryOwnerMixin, CollateAuthorAndPkMixin, generic.edit.UpdateView):
     fields = '__all__'
 
-class DeleteView(DiaryOwnerMixin, generic.edit.DeleteView):
-    model = Article 
-     
+class DeleteView(DiaryOwnerMixin, CollateAuthorAndPkMixin, generic.edit.DeleteView):
     def get_success_url(self):
         return reverse_lazy('diary:user_index', kwargs={'username': self.kwargs['username']})
-
-# def get_diary_owner(self, context):    
-#     username = self.kwargs['username']  
-#     diary_owner = CustomUser.objects.get(username=username)
-#     context['diary_owner'] = diary_owner
-#     return context 
-
-# def get_diary_owner_context(self, **kwargs):
-#     context = self.super().get_context_data(**kwargs)
-#     username = self.kwargs['username']  
-#     diary_owner = CustomUser.objects.get(username=username)
-#     context['diary_owner'] = diary_owner
-#     return context 
-
 
